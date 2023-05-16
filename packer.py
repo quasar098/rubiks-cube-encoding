051edd5868decc10c62a2df7cd58eb690ed57d71
quasar098 <patellmlg@gmail.com>
2023-05-15 20:49:12 -0400

    switcharoo!

--- packer.py
+++ packer.py
@@ -1,4 +1,4 @@
-from basil import rubiks_from_decimal
+from basil import rubiks_from_decimal, rubiks_to_decimal
 from enum import Enum
 
 rcencoding_alphabet = "abdefghijklmnprstuvwyz0123456789"
@@ -17,8 +17,24 @@ def encode_rcencoded_data_to_basil(data: str):
     return rubiks_from_decimal(bits)
 
 
+def decode_basil_to_rcencoded_data(corners: str, edges: str):
+    dec = rubiks_to_decimal(corners, edges)
+    length = (dec & 0b111 << 41) >> 41
+    parity = (dec & 0b1 << 40) >> 40
+    rest = (dec & 0b1111111111111111111111111111111111111111)
+    if sum(map(int, "{0:b}".format(rest))) ^ parity == 0:
+        raise NotImplemented("Parity failure!")
+    message = ""
+    for _ in range(length+1):
+        chunk_data = rest & 0b11111
+        rest = rest >> 5
+        message += rcencoding_alphabet[chunk_data]
+    return message[::-1]
+
+
 def main():
-    print(encode_rcencoded_data_to_basil("urkiddng"))
+    # print(encode_rcencoded_data_to_basil("urkiddng"))
+    print(decode_basil_to_rcencoded_data('GCFEHABD', 'CAILHBJKGDFE'))
 
 
 if __name__ == '__main__':
